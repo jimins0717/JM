6831f15c292bbe980bbae76478ee112ef1c950fc
jimins0717 <jimins0717@gmail.com>
2026-09-24 15:26:36 +0900

    Add files via upload

---
+++ 2만원 복구봇/설정.py
@@ -0,0 +1,17 @@
+봇_토큰 = "MTUxMTU4MTk0NjY5MjYzNjc1NA.G6dtq_.IdbdKMvcCIcjf2h-WjsGggVieJ4Ih_Oxhtp4GY" #봇의 토큰
+관리자아이디 = [748446781116776459] #라이센스 뽑을 수 있는 사람의 아이디
+API_엔드포인트 = "https://discord.com/api/v9"  #웬만해선 안건드려도됨
+클라이언트_아이디 = "1511581946692636754" #디스코드 개발자 센터 Oauth2 탭에 들어가면 있는 Client ID
+클라이언트_시크릿 = "EcgyTNca3XqSFZR1qnmMymc48_3gF4CV" #디스코드 개발자 센터 Oauth2 탭에 들어가면 있는 Client Secret
+도메인 = "http://restorebot.duckdns.org" #복구봇을 이용할 도메인
+복구로그웹훅 = "https://discord.com/api/webhooks/1552534408160878642/wxTv6kwCXPENGYgO-Ito9gbQhuaBaEFSdfD2oqMD3BvADA3g-nfhP2oFKBwqodCfkqO7"
+
+########### 건드리지 마세요 ###########
+token = 봇_토큰
+admin_id = 관리자아이디
+api_endpoint = API_엔드포인트
+client_id = 클라이언트_아이디
+client_secret = 클라이언트_시크릿
+base_url = 도메인
+bokweb =복구로그웹훅
+########### 건드리지 마세요 ###########
